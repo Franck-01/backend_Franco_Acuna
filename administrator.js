fe1b5067ab27bb278c058d5ef0c201eb66eda882
Franco Acuña <franconacuna5701@gmail.com>
2022-02-03 22:16:30 -0300

    router

--- administrator.js
+++ administrator.js
@@ -1,60 +1,49 @@
 const fs = require("fs");
 
-const pathToClones = "./archives/clones.json"
+const pathProducts = "./ecommers/products.txt"
 class Administrator {
-    saveClone = async(clone) => {
-        if (!clone.name || !clone.rank || !clone.num_serie) return { status: "error", error: "lost information" }
+    saveProduct = async(ship) => {
+        if (!ship.name || !ship.model || !ship.cost) return { status: "error", error: "informacion perdida" }
         try {
-            if (fs.existsSync(pathToClones)) {
-                let data = await fs.promises.readFile(pathToClones, "utf-8")
-                let clones = JSON.parse(data)
-                let id = clones[clones.length - 1].id + 1
-                clone.id = id
-                clones.push(clone)
-                await fs.promises.writeFile(pathToClones, JSON.stringify(clones, null, 2))
-                return { status: "sucess", message: "Clon created" }
+            if (fs.existsSync(pathProducts)) {
+                let data = await fs.promises.readFile(pathProducts, "utf-8")
+                let ships = JSON.parse(data)
+                let id = ships[ships.length - 1].id + 1
+                ship.id = id
+                ships.push(ship)
+                await fs.promises.writeFile(pathProducts, JSON.stringify(ships, null, 2))
+                return { status: "sucess", message: "Producto adquirido" }
             } else {
-                clone.id = 1
-                await fs.promises.writeFile(pathToClones, JSON.stringify([clone], null, 2))
-                return { status: "sucess", message: "Clon created" }
+                ship.id = 1
+                await fs.promises.writeFile(pathProducts, JSON.stringify([ship], null, 2))
+                return { status: "sucess", message: "Producto adquirido" }
             }
         } catch (error) {
             return { status: "error", message: error }
         }
     }
-
-    getById = async(id) => {
-        if (fs.existsSync(pathToClones)) {
-            let data = await fs.promises.readFile(pathToClones, "utf-8")
-            let clones = JSON.parse(data)
-            let clone = clones.find(u => u.id === id)
-            if (clone) return { status: "succes", clone: clone }
-            else return { status: "error", error: "clon not found" }
+    getProduct = async() => {
+        if (fs.existsSync(pathProducts)) {
+            let data = await fs.promises.readFile(pathProducts, "utf-8")
+            let ships = JSON.parse(data)
+            return { status: "success", load: ships }
         }
     }
+    randomProduct = async() => {
+        if (fs.existsSync(pathProducts)) {
+            let data = await fs.promises.readFile(pathProducts, "utf-8", null, 3)
+            let ships = JSON.parse(data)
 
+            let randoms = Math.floor(Math.random() * (ships.length) + 1)
+            return { status: "sucess", ship: randoms }
+        }
+    }
     getAll = async() => {
-        if (fs.existsSync(pathToClones)) {
-            let data = await fs.promises.readFile(pathToClones, "utf-8")
-            let clones = JSON.parse(data)
-            return { status: "success", load: clones }
+        if (fs.existsSync(pathProducts)) {
+            let data = await fs.promises.readFile(pathProducts, "utf-8")
+            let ships = JSON.parse(data)
+            return { status: "success", load: ships }
         }
-    }
-
-    deleteById = async(id) => {
-        if (!id) return { status: "error", error: "Id needed" }
-        if (fs.existsSync(pathToClones)) {
-            let data = await fs.promises.readFile(pathToClones, "utf-8")
-            let clones = JSON.parse(data)
-            let newClone = clones.filter(clone => clone.id !== id)
-            await fs.promises.writeFile(pathToClones, JSON.stringify(newClone, null, 2))
-            return { status: "success", message: "clon delete" }
-        }
-    }
-
-    deleteAll = () => {
-        fs.unlinkSync("./archives/clones.json")
-        return { status: "success", message: "clon delete" }
     }
 }
 
--- index2.js
+++ index2.js
@@ -1,26 +1,21 @@
 const Administrator = require("./administrator.js")
 const userServices = new Administrator
 
-class clone {
-    constructor(name, rank, designation, num_serie, squadron) {
+class ship {
+    constructor(name, model, cost, img_url) {
         this.name = name,
-            this.rank = rank,
-            this.designation = designation,
-            this.num_serie = num_serie,
-            this.squadron = squadron
+            this.model = model,
+            this.cost = cost,
+            this.img_url = img_url
     }
 }
 
-const clon = new clone("Rex", "Capitan", "clon de infanteria", "CT-7567", "Legion 501")
-const clon = new clone("Cody", "Comandante", "clon de infanteria", "CT-2224", "Batallon 212")
-const clon = new clone("Hunter", "Sargento", "clon de infanteria", "CT-9901", "Escuadron 99")
-const clon = new clone("Cincos", "Cabo", "clon de ARC", "CT-5555", "Legion 501")
-const clon = new clone("Yularen", "Almirante", "oficiales de armada", "null", "Legion 501")
-const clon = new clone("Gregor", "Capitan", "clon comando", "CT-5576", "batallon 212")
-const clon = new clone("Hawk", "Teniente", "piloto de la armada", "CT-8103", "Legion 501")
+//const repu = new ship("Venator", "Destructor Estelar", 59000000, "https://i.pinimg.com/originals/08/ae/6c/08ae6c35db460ca5c06cdb7e87eb07fa.jpg")
+//const repu = new ship("Arquitens", "Crucero Ligero", 4000000, "https://qph.fs.quoracdn.net/main-qimg-1446b508a15d9d0a668ad2a68ec8d223-pjlq")
+//const repu = new ship("ARC-170 Starfighter", "Caza de combate", 150000, "https://i.pinimg.com/originals/c9/21/b9/c921b926bf13588e2fb7203f21e53938.jpg")
+//const repu = new ship("Clone Head Hunter Z-95", "Caza de combate", 75000, "http://pm1.narvii.com/6245/0b971eb50fa1bcd128c4ebd2df779627e535c512_00.jpg")
+//const repu = new ship("(BTL-B) Ala-Y", "Bombardero", 140000, "https://media.contentapi.ea.com/content/dam/star-wars-battlefront-2/images/2019/08/swbf2-refresh-hero-medium-vehicles-page-btl-b-y-wing-7x2-xl.jpg.adapt.crop16x9.320w.jpg")
 
-userServices.saveClone(clon).then(result => console.log(result))
-userServices.getById(2).then(result => console.log(result))
+//userServices.saveProduct(repu).then(result => console.log(result))
 userServices.getAll().then(result => console.log(result))
-userServices.deleteById(3).then(result => console.log(result))
-userServices.deleteAll()
+userServices.randomProduct().then(result => console.log(result))

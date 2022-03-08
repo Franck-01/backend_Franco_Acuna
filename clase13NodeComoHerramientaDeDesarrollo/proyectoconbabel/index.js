b897910a48235227f9a0d602520f34f243a2aeb2
Franck-01 <87050434+Franck-01@users.noreply.github.com>
2022-03-08 16:53:37 -0600

    class14

--- clase13NodeComoHerramientaDeDesarrollo/proyectoconbabel/index.js
+++
@@ -1,3 +0,0 @@
-const array = [1,2,3,4,5]
-
-array.map(valor=>valor*2).forEach(valor=>console.log(valor))
--- clase13NodeComoHerramientaDeDesarrollo/typscriptconconfig/dest/index.js
+++
@@ -1,5 +0,0 @@
-import * as operaciones from './utils/operaciones.js';
-let numero1 = 2;
-let numero2 = 4;
-console.log(`La suma de ${numero1} más ${numero2} es ${operaciones.sumar(numero1, numero2)}`);
-console.log(`La resta de ${numero1} menos ${numero2} es ${operaciones.restar(numero1, numero2)}`);

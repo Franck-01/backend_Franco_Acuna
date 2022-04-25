bef87e23576182efc7f8683f391c46ec26526b03
Franco Acuña <franconacuna5701@gmail.com>
2022-04-25 16:30:10 -0300

    cookies monster-project_V.4

--- cookie monster-project/src/app.js
+++ cookie monster-project/src/app.js
@@ -5,10 +5,11 @@ const fileStrategy = require("session-file-store")
 const productsManager = require("./Managers/productsManagers.js")
 const { Server } = require("socket.io")
 const files = require("./files/products.json")
+let multer = require("multer")
 
 const servicesData = new productsManager()
 const app = express()
-const PORT = process.nextTick.PORT || 8080
+const PORT = process.env.PORT || 8000
 const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 const FileStorage = fileStrategy(session)
 const io = new Server(server)
@@ -18,6 +19,7 @@ app.set("files", __dirname + "/files")
 app.set("view engine", "ejs")
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(session({
     store: new FileStorage({ path: "./src/sessions", ttl: 3600, retries: 0 }),
@@ -28,8 +30,7 @@ app.use(session({
 
 app.get("/", (req, res) => {
     res.render("index.ejs", {
-        files,
-        users
+        files
     })
 })
 app.get("/product", (req, res) => {
@@ -41,31 +42,29 @@ app.post("/productsArray", (req, res) => {
     files.push(req.body)
     res.redirect("/product")
 })
-app.get("/user", (req, res) => {
-    if (req.session.user) return res.render("user.ejs", {
-        users
-    })
-    let { userCreate } = req.query
-    req.session.user = userCreate
-    res.redirect("/")
-    res.send(`Bienvenido ${userCreate}`)
+app.get("/login", (req, res) => {
+    let user = req.session.user
+    console.log(req.session)
+    if (user) return res.render("login.ejs")
 })
 
-let users = []
-
-app.post("/userLogin", (req, res) => {
-    const { userCreate } = req.body
-    req.session.user = userCreate
-    const userFound = users.find(us => us.userCreate === userCreate)
+app.post("/login", (req, res) => {
+    let data = req.body
+    req.session.user = data.username
+    const { username } = req.body;
+    const userFound = users.find(el => el.username === username);
     if (userFound) {
-        console.log("El usuario ya existe")
-        res.redirect("/")
+        console.log("el usuario ya existe")
     } else {
-        users.push(req.body)
-        req.session.user = req.body
-        res.redirect("/")
-        console.log("El usuario se a creado")
+        users.push(req.body);
+        req.session.user = req.body;
+        res.redirect('/')
     }
+    res.send(console.log("ok"))
+})
+app.get("/user", (req, res) => {
+    let user = req.session
+    res.send(user)
 })
 io.on("connection", async socket => {
     console.log("conection realizada")

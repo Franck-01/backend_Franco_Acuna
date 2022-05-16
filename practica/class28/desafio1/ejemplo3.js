e888df4d8f9bbe97903b3f5dc6f7dff4f5de51d0
Franco Acuña <franconacuna5701@gmail.com>
2022-05-16 11:12:55 -0300

    class28

--- practica/class28/desafio1/ejemplo3.js
+++
@@ -1,8 +0,0 @@
-process.stdout.write("Hola desde el programa \n");
-process.stdout.write("Cual es tu nombre?: ");
-
-process.stdin.on('data', data => {
-    console.log('mucho gusto ', data.toString())
-        //console.log("datos desde consola",data)   ---- (para codigo numeral)
-    process.exit();
-})

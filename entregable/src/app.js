10fed0c3d166d00967bac302d83c19012c1c35c3
Franco Acuña <franconacuna5701@gmail.com>
2022-05-31 23:09:16 -0300

    ad

--- entregable/src/app.js
+++ entregable/src/app.js
@@ -6,31 +6,14 @@ const app = express()
 const PORT = process.env.PORT || 8080
 const server = app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
 
-app.set('views', __dirname + '/views')
-app.set('routes', __dirname + '/routes')
-app.set('files', __dirname + '/files')
-app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-let carts = [];
-let ecommers = []
-
-app.get("/", (req, res) => {
-    res.render("index.ejs", {
-        carts,
-        ecommers
+app.use('/api/products', productRouter);
+app.use('/api/cart', cartRouter);
+app.get('*', (req, res) => {
+    res.send({
+        status: "error",
+        error: `Couldn't find route ${req.url}`
     })
-})
-app.get("/carts", (req, res) => {
-    res.render("carts.ejs", {
-        carts
-    })
-    app.use("/carts", cartRouter)
-})
-app.get("/products", (req, res) => {
-    res.render("products.ejs", {
-        ecommers
-    })
-    app.use("/products", productRouter)
 })

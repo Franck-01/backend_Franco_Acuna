a0615a480e34432a3d35a3b1cfb7d933c2dedf04
Franco Acuña <franconacuna5701@gmail.com>
2022-07-05 00:31:42 -0300

    http.V_2

---
+++ http/http_client/index.js
@@ -0,0 +1,28 @@
+const https = require("https")
+
+const url = "localhost:8080.com";
+
+const option = {
+  hostname: url,
+  port: 443,
+  path: "/todos",
+  method: "GET",
+};
+
+const request = https.request(option, (response) => {
+  let data = "";
+  response.on("data", (chunk) => {
+    data += chunk.toString();
+  });
+
+  response.on("end", () => {
+    const body = JSON.parse(data);
+    console.log(body);
+  });
+});
+
+request.on("error", (error) => {
+  console.log(error);
+});
+
+request.end();
